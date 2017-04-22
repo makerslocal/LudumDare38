c9963529c7b60bc2beca5b8b2bd9486a0a50cd50
Hunter Fuller <hfuller@pixilic.com>
2017-04-22 17:16:15 -0500

    Add scaffolding for skipping ease-in
    But it doesn't work yet. So we'll see if we bother with this one.

--- bees-in-the-trap/Assets/Scripts/MainCamera.cs
+++ bees-in-the-trap/Assets/Scripts/MainCamera.cs
@@ -18,23 +18,29 @@ public class MainCamera : MonoBehaviour {
 	}
 
 	public void scootTo(Vector3 endpos) {
+		double time = .6;
+		bool skipEaseIn = false;
 		if (currentMove != null) {
 			//if we're moving, fuck that
 			StopCoroutine (currentMove);
+			skipEaseIn = true;
 		}
 		Debug.Log ("Scoot TO:"); Debug.Log(endpos);
-		currentMove = SmoothMove (this.transform.position, endpos, 5.0);
+		currentMove = SmoothMove (this.transform.position, endpos, time, skipEaseIn);
 		StartCoroutine (currentMove);
 	}
 
-	IEnumerator SmoothMove(Vector3 startpos, Vector3 endpos, double seconds) {
+	IEnumerator SmoothMove(Vector3 startpos, Vector3 endpos, double seconds, bool skipEaseIn = false) {
 		double t = 0.0;
+		if (skipEaseIn) {
+			//DO SOMETHING???
+			//t = 0.1;
+		}
+
 		endpos.z = this.transform.position.z; //NEVER move forward or backward (Hack because we are in 2D)
 		while ( t <= 1.0 ) {
-			//Debug.Log ("t=" + t);
 			t += Time.deltaTime/seconds;
 			transform.position = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep((float) 0.0, (float) 1.0, (float) t));
-			//Debug.Log (transform.position);
 			yield return null; //WHY 
 		}
 	}
