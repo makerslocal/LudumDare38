9780114f327304c6d31183bbc95853843bec3409
Hunter Fuller <hfuller@pixilic.com>
2017-04-23 01:27:15 -0500

    Add ability to hide the tutorial

---
+++ bees-in-the-trap/Assets/Scripts/TutorialHider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialHider : MonoBehaviour {
+
+	public GameObject tutorial;
+	private bool isShowing = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("t")) {
+			isShowing = !isShowing;
+			tutorial.SetActive (isShowing);
+		}
+	}
+}
