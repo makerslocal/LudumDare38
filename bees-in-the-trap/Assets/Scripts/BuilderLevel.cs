fc34e2ef7feca53a9cbc9f689e2181511742c512
Hunter Fuller <hfuller@pixilic.com>
2017-04-22 20:52:38 -0500

    Add a level script that can trigger the ending cutscene

---
+++ bees-in-the-trap/Assets/Scripts/BuilderLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuilderLevel : MonoBehaviour {
+
+	public CameraManager camera;
+
+	public void doTakeoff() {
+		double time = 1.5;
+		Vector3 pos = camera.transform.position;
+		pos.y += -10;
+		//camera.scootTo (pos, time);
+		camera.rotateTo (new Vector3 (0, 0, 180), time);
+		camera.zoomTo (25, time);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("return")) {
+			doTakeoff ();
+		}
+	}
+}
